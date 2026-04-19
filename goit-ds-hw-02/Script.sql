037b1c7b93141c04892b7188643094cfa9ddceaf
jackyurchiksparrow <uradzbq@gmail.com>
2026-04-19 16:49:54 +0300

    started sql, updated git and docker ignore files
    omit Python Cache & Compiled Files from docker and git

---
+++ goit-ds-hw-02/Script.sql
@@ -0,0 +1,57 @@
+PRAGMA foreign_keys = ON; -- sets CASCADE command to work, it is disabled by default
+
+CREATE TABLE users (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	fullname VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	CONSTRAINT email_uq UNIQUE (email) -- we could just use 'email VARCHAR(100) NOT NULL UNIQUE', 
+										   -- but using constraints is the best practice for clarity, convenient change using ALTER
+	                                       -- and if the error occurs, it will refer to this specific 'email_uq' key that we can assign thanks to 'CONSTRAINT'
+	                                       -- + you can do combinations of constraints: 'UNIQUE (fullname, email)' not just for email
+);
+
+CREATE TABLE status (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	name VARCHAR(50) NOT NULL,
+	CONSTRAINT name_uq UNIQUE (name)
+);
+
+CREATE TABLE tasks (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	title VARCHAR(100) NOT NULL,
+	description TEXT NOT NULL,
+	status_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL,
+	CONSTRAINT status_id_fk FOREIGN KEY (status_id) REFERENCES status (id)
+		ON DELETE CASCADE  -- means if the original primary key is deleted (user), then all related things it was referring to will be deleted
+		                   -- if we wanted to just set them to null instead of deleting when the user is gone, the command would be 'ON DELETE SET NULL'
+		ON UPDATE CASCADE, -- means if the original primary key is changed (user), then all ids that references it will also be updated
+		                   -- they say it's extremely rare, but I find it very useful and would use it always. In fact, I believe it must be made default
+	CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+INSERT INTO users(fullname, email) VALUES("Anastasia Kolpakova", "kolpalova@ukr.net");
+INSERT INTO users(fullname, email) VALUES("Dmitryi Sober", "darkdeus@ukr.net");
+INSERT INTO users(fullname, email) VALUES("Daniil Bronycagevich", "bronycage@ukr.net");
+
+INSERT INTO status(name) VALUES("('new',)");
+INSERT INTO status(name) VALUES("('in progress',)");
+INSERT INTO status(name) VALUES("('completed',)");
+
+SELECT * FROM status;
+SELECT * FROM users;
+
+INSERT INTO tasks(title, description, status_id, user_id) VALUES("Bake pretzels", "Use the rye starter", 1, 3);
+INSERT INTO tasks(title, description, status_id, user_id) VALUES("Close nether", "All characters", 2, 1);
+INSERT INTO tasks(title, description, status_id, user_id) VALUES("Do not drink", "Do not drink alcohol", 3, 2);
+
+SELECT * FROM tasks;
+
+DELETE FROM users where id==2;
+
+SELECT * FROM tasks; -- "do not drink" is still there
+
+
+
